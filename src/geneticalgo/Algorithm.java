838c36aff1cac7f682c694b232a420900c8ecf6d
h3h3h0h0 <deboss.wt@gmail.com>
2023-01-27 09:32:48 -0500

    Some comments and stuff

--- src/geneticalgo/Algorithm.java
+++ src/geneticalgo/Algorithm.java
@@ -1,6 +1,21 @@
 package geneticalgo;
 
+import java.util.ArrayList;
+
 public class Algorithm<T extends Chromosome>{
+
+    protected ArrayList<T> population;
+    protected int generation;
+    //the population size to cull to
+    protected int popSize;
+    //the range of offspring numbers (i.e. the amount of new instances produced by a pair of chromosomes)
+    protected int minOffspring;
+    protected int maxOffspring;
+
+    //advance 1 generation
+    public void iterate(){
+
+    }
 
 
 
--- src/geneticalgo/Chromosome.java
+++ src/geneticalgo/Chromosome.java
@@ -1,7 +1,13 @@
 package geneticalgo;
 
-public abstract class Chromosome implements Comparable {
+import java.util.ArrayList;
 
+public abstract class Chromosome{
 
+    protected ArrayList<Object> genes;
+
+    public abstract double fitness(); //a number calculated to represent how good a particular chromosome is
+    public abstract Chromosome breed(Chromosome c); //a method that produces a single child based on parent genes
+    public abstract Chromosome mutate(); //simulation of mutation, randomly changes the genes in some way
 
 }
